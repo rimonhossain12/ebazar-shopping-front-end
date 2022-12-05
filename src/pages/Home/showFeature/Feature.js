30cdef466e93bc230b8ce4ca6c77529b93bfe560
rimonhossain12 <rimonmpi@gmail.com>
2022-12-05 15:07:13 +0600

    feature part added

--- src/pages/Home/showFeature/Feature.js
+++ src/pages/Home/showFeature/Feature.js
@@ -1,17 +1,13 @@
+import { Card } from "flowbite-react";
 import React from "react";
 
-const feature = ({feature}) => {
-  const {productName,productImage} = feature?.data;
-  console.log(productImage);
+const feature = ({ feature }) => {
+  const { images } = feature;
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <img className="rounded-t-lg" src={`productImage`}  alt="not response" />
-        {/* <img id="img-style" src={productImage} className='img-fluid' style={{ width: '100%' }} alt="" /> */}
-      <div className="p-5">
-          <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-           {productName}
-          </h5>
-      </div>
+    <div className="max-w-sm">
+     <Card>
+      <img style={{height:'180px'}} src={images} alt="not found" />
+      </Card>
     </div>
   );
 };
--- src/pages/Home/showFeature/ShowFeature.js
+++ src/pages/Home/showFeature/ShowFeature.js
@@ -1,27 +1,52 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Feature from "./Feature";
-const ShowFeature = () => {
-  const [features, setFeature] = useState([]);
+import cloth from '../../../images/feature/cloth.jpg';
+import laptop from '../../../images/feature/laptop.jpg';
+import perfume from '../../../images/feature/perfume.jpg';
+import phone from '../../../images/feature/phone.jpg';
+import shari from '../../../images/feature/shari.jpg';
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/feature")
-      .then((res) => {
-        // console.log(res.data)
-        const data = res.data;
-        setFeature(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+
+const featureProducts = [
+  {
+    _id:1,
+    name:'shari',
+    images:shari
+  },
+  
+  {
+    _id:2,
+    name:'cloth',
+    images:cloth
+  },
+  {
+    _id:3,
+    name:'phone',
+    images:phone
+  },
+  {
+    _id:4,
+    name:'perfum',
+    images:perfume
+  },
+  {
+    _id:5,
+    name:'laptop',
+    images:laptop
+  },
+
+]
+
+
+const ShowFeature = () => {
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5 container-lg px-12">
-      {features.map((feature) => (
-        <Feature key={feature._id} feature={feature} />
-      ))}
+    <div className="container-lg my-12">
+          <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 px-12 mx-auto">
+            {
+              featureProducts.map((feature) => <Feature  key={feature._id} feature={feature} />)
+            }
+          </div>
     </div>
   );
 };

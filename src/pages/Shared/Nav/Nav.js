36394a5b7564dc8a2eaf1909d85c9d24372841e4
rimonhossain12 <rimonmpi@gmail.com>
2022-12-07 01:12:33 +0600

    added cart icon with position

--- src/pages/Shared/Nav/Nav.js
+++ src/pages/Shared/Nav/Nav.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { Navbar,Button } from "flowbite-react";
+import { Navbar, Button } from "flowbite-react";
 import logo from "../../../images/navbar/commer-logo.png";
 import { Link } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
-import { signOut } from 'firebase/auth';
-
-// FaShoppingCart
-
-
+import { signOut } from "firebase/auth";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Nav = () => {
   const [user] = useAuthState(auth);
@@ -29,16 +26,29 @@ const Nav = () => {
           </Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse>
-          <Link to="/">Home</Link>         
-            <Link to="/about" className="fw-bold text-capitalize fs-7">About</Link>
-            <Link to="/contact" className="fw-bold text-capitalize fs-7">Contact</Link>
-            {/* <Link to="/dashboard" className="fw-bold text-capitalize fs-7">Dashboard</Link> */}
-            {/* <Link to="/feature" className="fw-bold text-capitalize fs-7">Feature-product</Link> */}
-            {
-             user?.email ?  <Button gradientDuoTone="purpleToPink" onClick={logout}>
-             Logout
-           </Button>:   <Link to="/login" className="fw-bold text-capitalize fs-7">Login</Link>
-           }
+            <Link to="/">Home</Link>
+            <Link to="/about" className="fw-bold text-capitalize mb-3 fs-7">
+              About
+            </Link>
+            <Link to="/contact" className="fw-bold text-capitalize mb-3 fs-7">
+              Contact
+            </Link>
+            {user?.email ? (
+              <Button gradientDuoTone="purpleToPink" onClick={logout}>
+                Logout
+              </Button>
+            ) : (
+              <Link to="/login" className="fw-bold text-capitalize mb-3 fs-7">
+                Login
+              </Link>
+            )}
+            <Link to="/contact" className="fw-bold text-capitalize mb-3 fs-7">
+            <span className="text-red-500 absolute mt-[-12px] ml-[12px] font-bold">10</span>
+              <FaShoppingCart 
+              className="relative"
+              size={25} />
+               
+            </Link>
           </Navbar.Collapse>
         </Navbar>
       </div>

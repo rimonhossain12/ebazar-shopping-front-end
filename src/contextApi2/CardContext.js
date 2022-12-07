3fdcd063ef162c73f458b4fcfe54200aa75703c6
rimonhossain12 <rimonmpi@gmail.com>
2022-12-07 14:13:29 +0600

    set quantiy

--- src/contextApi2/CardContext.js
+++ src/contextApi2/CardContext.js
@@ -9,31 +9,35 @@ function CardContextProvider({ children }) {
 
   useEffect(() => {
     const getCart = localStorage.getItem('cart');
-    if ( getCart ){
+    if (getCart) {
       setCart(JSON.parse(getCart));
     }
   }, []);
 
   const handleAddToCart = (data) => {
-    const findIndex = cart.findIndex((item) => item.id === data.it);
-    if (findIndex !== -1) {
-      cart[findIndex].quantity = cart[findIndex].quantity
-        ? (cart[findIndex].quantity += 1)
-        : 1;
-      setCart([...cart, data]);
-    } else {
+    const findIndex = cart.findIndex((item) => item.id === data.id);
+
+    if ( findIndex !== -1 ){
+      // cart[findIndex].qty = cart[findIndex].qty ? cart[findIndex].qty + 1 : 1;
+      cart[findIndex].qty = cart[findIndex].qty ? cart[findIndex].qty + 1 : 1;
+
+      setCart([...cart],cart[findIndex]);
+    }
+    else {
       setCart([
         ...cart,
         {
-          quantity: 1,
-        },
-      ]);
+          ...data,
+          qty:1
+        }
+      ])
     }
 
-    localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem('cart',JSON.stringify(cart));
+
   };
 
-  console.log("cart products = ", cart);
+  console.log('user cart = ',cart);
 
   return (
     <CardContext.Provider value={{ cart, handleAddToCart }}>
--- src/pages/Home/MyOrder/MyOrder.js
+++ src/pages/Home/MyOrder/MyOrder.js
@@ -1,20 +1,40 @@
+import { Card } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 
 const MyOrder = () => {
-  const [item,setItem] = useState([]);
+  const [items,setItems] = useState([]);
 
   useEffect(() => {
     const cartData = localStorage.getItem('cart');
     if(cartData){
-      setItem(JSON.parse(cartData));
+      setItems(JSON.parse(cartData));
     }
   },[])
 
-  console.log(item);
+  console.log(items.qty);
 
   return (
     <div className='contianer-lg px-12 my-12 '>
-        <h3 className="text-3xl text-gray-500">All the my order : {item.length}</h3>
+        <h3 className="text-3xl text-gray-500">All the my order : {items.length}</h3>
+        <div className='grid grid-cols-1 mb-5'>
+          {
+            items.map((item) => (
+              <div className="max-w-sm">
+              <Card
+                horizontal={true}
+                imgSrc={item?.image}
+              >
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Noteworthy technology acquisitions 2021
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                </p>
+              </Card>
+            </div>
+            ))
+          }
+        </div>
     </div>
   )
 }
